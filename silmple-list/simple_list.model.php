0e65b8ed9ab7e950b6ef99e5b72eeec247cb9355
Maksim Zaykov <8859590+mszaykov@users.noreply.github.com>
2025-01-10 17:31:25 +0100

    Code formatting.

--- silmple-list/simple_list.model.php
+++ silmple-list/simple_list.model.php
@@ -2,7 +2,7 @@
 class SimpleList extends Model
 {
 	protected $name = 'Simple list';
-	
+
 	protected $model_elements = [
 		['Active', 'bool', 'active', ['on_create' => true]],
 		['Name', 'char', 'name', ['required' => true]],
@@ -26,14 +26,14 @@ class SimpleList extends Model
 	 * P.S. You can use the initial MV build and media/css/intro.css file to check the result.
  	*/
 
-    public function display(): string
+	public function display(): string
 	{
-        $rows = $this -> select(['active' => 1, 'order->asc' => 'position']);
+		$rows = $this->select(['active' => 1, 'order->asc' => 'position']);
 		$html = '';
-		
-		foreach($rows as $row)
-            $html .= "<li>".$row['name']."</li>\n";
 
-        return $html;
-    }
+		foreach ($rows as $row)
+			$html .= "<li>" . $row['name'] . "</li>\n";
+
+		return $html;
+	}
 }
